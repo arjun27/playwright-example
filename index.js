27fcbc17585d1214f4ddf73881005da1e60054b6
Arjun Attam <arjunattam@gmail.com>
2020-05-11 12:49:28 -0700

    re-trigger

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const { chromium } = require('playwright-chromium');
 
 (async() => {
+    //
     const browser = await chromium.launch();
     const page = await browser.newPage();
     await page.goto('https://example.com');
